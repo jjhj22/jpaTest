a85981db1d325eec554fe0b268b53ac2d99a3471
jjhj22 <skarnstktn@naver.com>
2024-08-20 13:41:40 +0900

    hi

--- src/main/java/com/jpaTest/control/BoardController.java
+++ src/main/java/com/jpaTest/control/BoardController.java
@@ -1,7 +1,14 @@
 package com.jpaTest.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
+
+    // localhost:8080
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
 }
